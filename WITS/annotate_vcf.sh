977f75b2e800d7ae7c841670bdccff22f8513405
Mamana <mypandos@gmail.com>
2018-11-07 14:44:38 +0200

    Fix config file

--- WITS/annotate_vcf.sh
+++ WITS/annotate_vcf.sh
@@ -4,10 +4,10 @@ HOMEDIR="/home/mamana/GAPW/main"
 OUTDIR="/spaces/mamana/GAPW"
 
 ### Nextflowscript here
-mdkir -p ${OUTDIR}/LOG
+mkdir -p ${OUTDIR}/LOG
 cd ${OUTDIR}
 nextflow -log ${OUTDIR}/LOG/annotate_vcf.log \
-    run mypandos \
+    run mypandos/annotate_vcf/main.nf \
     -w ${OUTDIR}/work \
     -resume \
     -profile pbs
